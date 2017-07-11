082fc894eed7484c1b4d99d5d3240580ea435ac7
devantonio25 <antonio.ae91@gmail.com>
2017-07-11 19:30:08 -0400

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -5,16 +5,34 @@
 //using a node module//https://nodejs.org/api/path.html#path_path_join_paths
 //go straight from the project folder right in tothe public folder 
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname + '/../public');
 const port = process.env.PORT || 3000;//configure heroku
 var app = express();
+var server = http.createServer(app);//express is intergrated with http//making this change so the server can use socket.io
+var io = socketIO(server);//passing in the server we want to use with our web sockets//we get back our web sockets server//this is how we will communicate with the server and the clients
 
 app.use(express.static(publicPath));
 
+//we can listen for a specific event and do something when that event happens
+//io.on lets you register an event listener
+//one popular built in event is called connection this lets you listen for a new connection 
+//meaning a client connected to the server 
+//and it lets you do something when that connection comes in
 
+io.on('connection', (socket) => {//socket//represents an individual socket as oppose to all the users connected to the server
+	//do something when the connection comes in 
+//everytime a user connects to our app we can print a message to the console
+	console.log('New user connected');
 
-app.listen(port, () => {
+	socket.on('disconnect', () => {
+		console.log('Client disconnected');
+	});
+});
+
+server.listen(port, () => {
 	console.log(`started up on port ${port}`);
 });
